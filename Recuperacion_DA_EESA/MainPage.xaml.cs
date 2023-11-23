996a81e2440708cc80e1577998d201cb40f4076f
Eclud <elramirosalas400@gmail.com>
2023-11-23 14:10:52 -0700

    Primer

---
+++ Recuperacion_DA_EESA/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Recuperacion_DA_EESA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //string Totalcuenta;
+        //string Porcentaje;
+        //string Personas;
+        //string Resultado;
+
+        //public void Calculo(ClickedEventArgs clickedEventArgs)
+        //{
+            
+
+
+        //}
+    }
+}
---
+++ Recuperacion_DA_EESA/datos/datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recuperacion_DA_EESA.datos
+{
+    public class datos
+    {
+        public datos() 
+        { 
+            
+        }
+    }
+}
